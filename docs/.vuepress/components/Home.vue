2f40143d374fe8b693ed14d3ba01d0102a159671
Gyuseok Kim <gyuseok6394@gmail.com>
2022-08-06 00:54:50 +0900

    update Home.vue

--- docs/.vuepress/components/Home.vue
+++ docs/.vuepress/components/Home.vue
@@ -12,7 +12,6 @@
       <div style="font-weight: bold; font-size: 2.2em; line-height: 1.2em;">Tech Interview for developer</div>
       <h4>👶🏻 신입 개발자 전공 지식 & 기술 면접 백과사전 📖</h4>
       <h5 style="color: gray;"> [제작] : Gyuseok Kim &nbsp; (Email : gyuseok6394@gmail.com)<br><br></h5>
-      <iframe src="https://github.com/sponsors/gyoogle/card" title="Sponsor gyoogle" height="225" width="600" style="border: 0;"></iframe>
       <a href="https://github.com/gyoogle/tech-interview-for-developer" target="_blank"> [⭐️GitHub Repository Link⭐️] </a> <br/><br/>
       <a href="https://github.com/gyoogle/tech-interview-for-developer/watchers"><img src="https://img.shields.io/github/watchers/gyoogle/tech-interview-for-developer.svg?style=social"/></a>
       <a href="https://github.com/gyoogle/tech-interview-for-developer/stargazers"><img src="https://img.shields.io/github/stars/gyoogle/tech-interview-for-developer.svg?style=social"/></a>
@@ -22,11 +21,9 @@
       <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgyoogle%2Fhit-counter&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
       <a href="#contributors"><img src="https://img.shields.io/badge/all_contributors-53-orange.svg?style=flat-square"/></a>
       <a href="http://makeapullrequest.com"><img src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square"/></a>
-      <br>
-      <br>
-      <a href="https://www.buymeacoffee.com/gyoogle" target="_blank">
-      <img src="./../public/images/BMC.png" width="180px" height="50px">
-      </a>
+      <br/>
+      <br/>
+      <iframe src="https://github.com/sponsors/gyoogle/card" title="Sponsor gyoogle" height="225" width="600" style="border: 0;"></iframe>
       <br/><br/>
     <h5>✔️주관적인 정리 내용으로 잘못된 정보가 있을 수 있습니다✔️
       <br><br>💡 해당 게시글 댓글이나 메일로 알려주세요! 💡</h5>
