e969249c2100bf59d828f0822d68c575c1da9a13
Gyoogle <gyuseok6394@gmail.com>
2024-07-07 11:11:39 +0900

    Update Home.vue

--- docs/.vuepress/components/Home.vue
+++ docs/.vuepress/components/Home.vue
@@ -50,6 +50,17 @@
 </template>
 
 <script>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  const script = document.createElement('script');
+  script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5225400119493497';
+  script.async = true;
+  script.setAttribute('crossorigin', 'anonymous');
+
+  document.head.appendChild(script);
+});
+
 </script>
 
 <style>
